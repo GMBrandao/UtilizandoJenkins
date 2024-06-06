5670f413bdd643ec9945101917c7799d44a6b970
GMBrandao <gabrielmartbrandao@gmail.com>
2024-06-06 04:00:06 -0300

    alteracao para testar pipeline jenkins

--- src/main/java/br/com/uniara/dpi2/gabriel/GabrielApplication.java
+++ src/main/java/br/com/uniara/dpi2/gabriel/GabrielApplication.java
@@ -9,5 +9,5 @@ public class GabrielApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GabrielApplication.class, args);
 	}
-
+	// adding a comment to verify updates on jenkins
 }
